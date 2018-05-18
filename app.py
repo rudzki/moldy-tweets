707e7a544ce770c6438db39536f6ac7ef2c3358e
Chris Rudzki <chris@rudzki.org>
2018-05-17 23:52:47 -0400

    clean up a little

--- app.py
+++ app.py
@@ -1,12 +1,6 @@
 from datetime import datetime
-import json
 import tweepy
 from creds import TWITTER_KEY, TWITTER_KEY_SECRET, TWITTER_TOKEN, TWITTER_TOKEN_SECRET
-
-# format 2018-05-12 17:18:34
-# dt object directives:
-# %Y-%m-%d %H:%M:%S
-#api.destroy_status(status_id)
 
 # twitter api object
 
@@ -16,11 +10,19 @@ api = tweepy.API(auth)
 
 def removeTweets():
     ''' Removes tweets older than 24 hours '''
+
+    # check the last hundred tweets; you should do an initial purge and not tweet more than 100 times a day 
     tweets = api.user_timeline(count=100)
+
     current_time = datetime.now()
+
     if tweets:
         for tweet in tweets:
-            created_at = datetime.strptime(str(tweet.created_at), '%Y-%m-%d %H:%M:%S')
+
+            # tweet.created_at returns string like '2018-05-12 17:18:34'
+            # let's cast this as a datetime object instead, and find the delta between now and then
+
+            created_at = datetime.strptime(tweet.created_at, '%Y-%m-%d %H:%M:%S')
             delta =  current_time - created_at
 
             # 86400 seconds in a day
