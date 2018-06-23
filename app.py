811c864ee68336357a9bf4cbb4fbdcf3b8a51395
Chris Rudzki <chris@rudzki.org>
2018-06-22 23:46:09 -0400

    abstract tweet age and add messaging

--- app.py
+++ app.py
@@ -8,23 +8,18 @@ auth = tweepy.OAuthHandler(TWITTER_KEY, TWITTER_KEY_SECRET)
 auth.set_access_token(TWITTER_TOKEN, TWITTER_TOKEN_SECRET)
 api = tweepy.API(auth)
 
-def removeTweets():
-    ''' Removes tweets older than 24 hours '''
-
-    # check the last hundred tweets
-    # you should do an initial purge
-    # and not tweet more than 100 times per 24 hours
-    # which would indicate a problem that no script could fix
+def removeTweets(tweet_age):
+    ''' Removes tweets older than tweet_age (in seconds) '''
 
     tweets = api.user_timeline(count=100)
-
     current_time = datetime.utcnow()
 
     if tweets:
         for tweet in tweets:
             delta =  current_time - tweet.created_at
-            if delta.total_seconds() > 86400: # 86400 seconds = 24 hours
+            if delta.total_seconds() > tweet_age:
                 api.destroy_status(tweet.id)
-                # print('Deleted ' + tweet.text)
+                print('Deleted:', tweet.text)
 
-removeTweets()
+removeTweets(86400)
+# remove tweets that are older than a day
