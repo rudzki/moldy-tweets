dc578b62c9b5b9d552f1085a8d55ea03767f93cd
rudzki <chris@rudzki.org>
2021-03-07 23:34:27 -0500

    updated to use python-twitter api

--- app.py
+++ app.py
@@ -1,24 +1,29 @@
-from datetime import datetime
-import tweepy
+from datetime import datetime, timedelta, timezone
+import twitter
 from creds import TWITTER_KEY, TWITTER_KEY_SECRET, TWITTER_TOKEN, TWITTER_TOKEN_SECRET
+from dateutil import tz
 
 # twitter api object
 
-auth = tweepy.OAuthHandler(TWITTER_KEY, TWITTER_KEY_SECRET)
-auth.set_access_token(TWITTER_TOKEN, TWITTER_TOKEN_SECRET)
-api = tweepy.API(auth)
+api = twitter.Api(consumer_key=TWITTER_KEY,
+                  consumer_secret=TWITTER_KEY_SECRET,
+                  access_token_key=TWITTER_TOKEN,
+                  access_token_secret=TWITTER_TOKEN_SECRET)
+
 
 def removeTweets(tweet_age):
     ''' Removes tweets older than tweet_age (in seconds) '''
 
-    tweets = api.user_timeline(count=100)
-    current_time = datetime.utcnow()
+    statuses = api.GetUserTimeline(count=200)
+    current_time = datetime.now().replace(tzinfo=tz.gettz("UTC"))
 
-    if tweets:
-        for tweet in tweets:
-            delta =  current_time - tweet.created_at
+    if statuses:
+        for tweet in statuses:
+            created_at = datetime.strptime(tweet.created_at, '%a %b %d %H:%M:%S %z %Y')
+            delta =  current_time - created_at
+
             if delta.total_seconds() > tweet_age:
-                api.destroy_status(tweet.id)
+                api.DestroyStatus(tweet.id)
                 print('Deleted:', tweet.text)
 
 removeTweets(86400)
